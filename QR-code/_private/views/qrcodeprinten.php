7cf612cddf3278f30a1eb6c58339a5c28e3db507
Thijn <thijnglas@gmail.com>
2022-01-23 10:58:10 +0100

    code afgekregen

---
+++ QR-code/_private/views/qrcodeprinten.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>qrPrinten</title>
+    <link rel="stylesheet" href="<?php echo site_url( '/css/QR.css' ) ?>" media="all">
+</head>
+<body>
+<section class="page">
+	<?php
+	$start  = 10000;
+	for ( $i = 1; $i <= 100; $i ++ ) {
+	    $nummer = $start + $i;
+        $color_num = 3 - ($i % 3);
+	    ?>
+        <figure class="code bg-color-<?php echo $color_num ?>">
+            <img src="<?php echo( '/qrcodes/qrcode-' . $nummer . '.svg' ); ?>"/>
+        </figure>
+        <?php if ( $i % 12 === 0 ): ?>
+            </section>
+            <section class="page">
+        <?php endif; ?>
+	<?php } ?>
+</section>
+</body>
+</html>
